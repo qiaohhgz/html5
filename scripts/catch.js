8d4c422413fd807492a93d212231eb7ae88541fa
jim_qiao <qiaohhgz@163.com>
2013-10-29 14:04:48 -0700

    update files

--- scripts/catch.js
+++ scripts/catch.js
@@ -1,13 +1,13 @@
-var d,i,j,n;
+var d, i, j, n;
 d = c("div");
-for(n=0;n<3;n++){
-for(j=0;j<7;j++){
-i = c("img");
-i.src = "Images/"+j+".jpg";
-d.appendChild(i);
-}
+for (n = 0; n < 3; n++) {
+    for (j = 0; j < 7; j++) {
+        i = c("img");
+        i.src = "Images/" + j + ".jpg";
+        d.appendChild(i);
+    }
 }
 document.getElementById("Layer1").appendChild(d);
-function c(el){
-return document.createElement(el);
+function c(el) {
+    return document.createElement(el);
 }
